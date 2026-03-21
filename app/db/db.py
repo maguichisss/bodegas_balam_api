267b3924ac29d751870d73bb92ae8cd690ca9b71
maguichisss <maguichisss@gmail.com>
2026-03-20 20:41:37 -0600

    changes to support multiple environments and use env vars

--- app/db/db.py
+++ app/db/db.py
@@ -3,7 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:password@localhost/bodegas_balam")
+DATABASE_URL = os.getenv("DATABASE_URL", "")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
